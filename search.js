d43cbefc06da007ceb88e15c81a68fddb1b2f766
SoundSlab <administrator@Mac-mini.local>
2020-09-30 11:54:51 -0400

    2

--- search.js
+++ search.js
@@ -1,6 +1,7 @@
-function binarySearch(array, value, start, end) {
+function binarySearch(array, value, start, end, c=0) {
     var start = start === undefined ? 0 : start;
     var end = end === undefined ? array.length : end;
+    let count = c;
 
     if (start > end) {
         return  -1;
@@ -9,18 +10,34 @@ function binarySearch(array, value, start, end) {
     const index = Math.floor((start + end)/ 2);
     const item = array[index];
 
-    console.log(start, end);
     if (item == value) {
-        return index;
+        console.log('binary:', count)
     }
     else if (item < value) {
-        return binarySearch(array, value, index + 1, end);
+        count++
+        return binarySearch(array, value, index + 1, end, count);
     }
     else if (item > value) {
-        return binarySearch(array, value, start, index - 1);
+        count++
+        return binarySearch(array, value, start, index - 1, count);
     }
 };
 
-let array = [3,5,6,8,11,12,14,15,17,18]
+function linearSearch(array, value) {
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+        count++
+        if (array[i] == value) {
+            console.log('linear: ', count)
+            return i;
+        }
+    }
+    
+}
 
-binarySearch(array, 16);
+let array = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 15, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
+
+array = array.sort();
+
+binarySearch(array, 78);
+linearSearch(array, 78);
